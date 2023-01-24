e79107fbf708e21a850577d3b339c066267fe28e
Michael Manley <michaelmanley@Michaels-MBP.lan>
2023-01-24 17:42:03 -0500

    update

--- server.js
+++ server.js
@@ -2,21 +2,12 @@
 const express = require('express')
 const app = express()
 const { Sequelize } = require('sequelize')
-
-// SEQUELIZE CONNECTION
-const sequelize = new Sequelize({
-    storage: process.env.PG_URI,
-    dialect: 'postgres',
-    username: 'postgres',
-    password: 'my_password'
-  })
-  
+const bands = require('./controllers/bands_controller')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
 
 // ROOT
 app.get('/', (req, res) => {
@@ -24,6 +15,16 @@ app.get('/', (req, res) => {
         message: 'Welcome to the Tour API'
     })
 })
+
+// CONTROLLERS  
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+
+const eventsController = require('./controllers/events_controller')
+app.use('/events', eventsController)
+
+const stagesController = require('./controllers/stages_controller')
+app.use('/stages', stagesController)
 
 // LISTEN
 app.listen(process.env.PORT, () => {
