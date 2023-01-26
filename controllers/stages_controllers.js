6a60cb771d74e760b4a472bfe916fd7fba5d4ce2
Michael Manley <michaelmanley@Michaels-MBP.lan>
2023-01-26 14:47:14 -0500

    updated stages controllers

--- controllers/stages_controllers.js
+++ controllers/stages_controllers.js
@@ -1,7 +1,7 @@
 // DEPENDENCIES
 const stages = require('express').Router()
 const db = require('../models')
-const { Stage } = db 
+const { Stage, Event } = db 
 const { Op } = require('sequelize')
 
 // FIND ALL STAGES
@@ -22,7 +22,16 @@ stages.get('/', async (req, res) => {
 stages.get('/:id', async (req, res) => {
     try {
         const foundStage = await Stage.findOne({
-            where: { stage_id: req.params.id }
+            where: { stage_id: req.params.id },
+            include:{ 
+                model: Event, 
+                as: "events",
+                through: { attributes: [] }
+            },
+            order: [
+                [{ model: Event, as: "events" }, 'date', 'ASC'],
+            ]
+
         })
         res.status(200).json(foundStage)
     } catch (error) {
@@ -35,7 +44,7 @@ stages.post('/', async (req, res) => {
     try {
         const newStage = await Stage.create(req.body)
         res.status(200).json({
-            message: 'Successfully inserted a new stage',
+            message: 'Successfully the new stage',
             data: newStage
         })
     } catch(err) {
