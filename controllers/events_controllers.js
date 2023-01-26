a0556b0bcd707fe3d0c8e5216af11e0a7f845c30
Michael Manley <michaelmanley@Michaels-MBP.lan>
2023-01-26 14:43:51 -0500

    updated events controllers

--- controllers/events_controllers.js
+++ controllers/events_controllers.js
@@ -1,7 +1,7 @@
 // DEPENDENCIES
 const events = require('express').Router()
 const db = require('../models')
-const { Event } = db 
+const { Event, MeetGreet, SetTime, Stage, Band } = db 
 const { Op } = require('sequelize')
 
 // FIND ALL EVENTS
@@ -23,7 +23,32 @@ events.get('/', async (req, res) => {
 events.get('/:id', async (req, res) => {
     try {
         const foundEvent = await Event.findOne({
-            where: { event_id: req.params.id }
+            where: { name: req.params.name },
+            include: [
+                { 
+                    model: MeetGreet, 
+                    as: "meet_greets", 
+                    attributes: { exclude: [ "event_id", "band_id" ] },
+                    include: {
+                         model: Band, 
+                         as: "band", 
+                    } 
+                },
+                { 
+                    model: SetTime, 
+                    as: "set_times",
+                    attributes: { exclude: [ "event_id", "stage_id", "band_id" ] },
+                    include: [
+                        { model: Band, as: "band" },
+                        { model: Stage, as: "stage" }
+                    ]
+                },
+                { 
+                    model: Stage, 
+                    as: "stages",
+                    through: { attributes: [] }
+                }
+            ]
         })
         res.status(200).json(foundEvent)
     } catch (error) {
